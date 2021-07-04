c9dea2be6ce0bdd1d24112d83a346dc8e0077204
Amin Husni <aminhusni@gmail.com>
2021-07-04 08:22:55 +0800

    Update __init__.py
    Change line color
    Change date format
    Add last data date
    Move JS into single JS file

--- DailyTrigger/__init__.py
+++ DailyTrigger/__init__.py
@@ -21,11 +21,13 @@ def main(mytimer: func.TimerRequest) -> None:
     # Get the current generation time in MYT timezone
     timeZ_My = pytz.timezone('Asia/Kuala_Lumpur')
     now = datetime.datetime.now(timeZ_My)
-    current_time = now.strftime("%d/%m/%Y %H:%M:%S")
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
     # Get datapoints from official CITF Malaysia and process as CSV with Pandas
     url = "https://raw.githubusercontent.com/CITF-Malaysia/citf-public/main/vaccination/vax_malaysia.csv"
     df = pd.read_csv(url)
+
+    last_date_data = df['date'].iloc[-1:].item()
 
     # Plot the graph
     vaccination_rate = px.line(df, x = 'date', y = 'total_daily',  
@@ -34,12 +36,16 @@ def main(mytimer: func.TimerRequest) -> None:
                                     "total_daily": "Daily doses"
                                 },
                                 title='Daily Vaccination Rate (Doses)')
+
+    vaccination_rate.update_traces(line_color='#1f822c')
+
     vaccinated_total = px.line(df, x = 'date', y = 'total_cumul',
                                 labels={
                                     "date": "",
                                     "total_cumul": "Daily doses"
                                 },
                                 title='Total Vaccination Dose')
+    vaccinated_total.update_traces(line_color='#1f822c')
 
 
     single_dose_total = df['dose1_cumul'].iloc[-1:].item()
@@ -51,6 +57,7 @@ def main(mytimer: func.TimerRequest) -> None:
                         ]
 
     df2 = pd.DataFrame(progress_frame)
+    # Pie chart
     progress_total = px.pie(df2, title='Vaccination Progress (at least 1 dose)', values='total', names='type', color='type',
                             color_discrete_map={
                                 'Vaccinated': '#3cb64c',
@@ -67,6 +74,7 @@ def main(mytimer: func.TimerRequest) -> None:
                         ]
 
     df3 = pd.DataFrame(progress2_frame)
+    # Pie chart
     progress2_total = px.pie(df3, title='Vaccination Progress (2 doses)', values='total', names='type', color='type',
                             color_discrete_map={
                                 'Vaccinated': '#3cb64c',
@@ -76,10 +84,10 @@ def main(mytimer: func.TimerRequest) -> None:
 
 
     # Convert plotted graph into HTML div
-    daily_rate_plot = vaccination_rate.to_html(full_html=False)
-    daily_rate_plot2 = vaccinated_total.to_html(full_html=False)
-    progress_plot = progress_total.to_html(full_html=False)
-    progress2_plot = progress2_total.to_html(full_html=False)
+    daily_rate_plot = vaccination_rate.to_html(full_html=False, include_plotlyjs=False)
+    daily_rate_plot2 = vaccinated_total.to_html(full_html=False, include_plotlyjs=False)
+    progress_plot = progress_total.to_html(full_html=False, include_plotlyjs=False)
+    progress2_plot = progress2_total.to_html(full_html=False, include_plotlyjs=False)
 
     # Generate day name based on date
     df['date'] = pd.to_datetime(df['date'])
@@ -95,7 +103,7 @@ def main(mytimer: func.TimerRequest) -> None:
                         title='Doses administed by day distribution')
 
     # Convert plotted graph into HTML div
-    day_trend_plot = day_trend.to_html(full_html=False)
+    day_trend_plot = day_trend.to_html(full_html=False, include_plotlyjs=False)
 
     # Get datapoints for per state in Malaysia
     url = "https://raw.githubusercontent.com/CITF-Malaysia/citf-public/main/vaccination/vax_state.csv"
@@ -108,13 +116,14 @@ def main(mytimer: func.TimerRequest) -> None:
                                 "total_cumul": "Doses",
                                 "state": "State",
                             },
+                            
                             title='Doses administed by state',
 
                             orientation='h')
-    state_plot = state_progress.to_html(full_html=False)
+    state_plot = state_progress.to_html(full_html=False, include_plotlyjs=False)
 
     # Crude HTML templates
-    HeadTemplate = '<!DOCTYPE html><html> <head><script async src="https://www.googletagmanager.com/gtag/js?id=G-JM59LT7FPT"></script><script>window.dataLayer=window.dataLayer || []; function gtag(){dataLayer.push(arguments);}gtag(\'js\', new Date()); gtag(\'config\', \'G-JM59LT7FPT\');</script> <meta name="viewport" content="width=device-width, initial-scale=1.0"><meta property="og:image" content="https://kururugi.blob.core.windows.net/kururugi/screenshot.jpg"/><meta property="og:title" content="Project Kururugi"/><meta property="og:description" content="Analysis and plotting of the official vaccination statistics data from JKJAV Malaysia"/> <style>*{box-sizing: border-box;}.header{padding: 15px;}.row::after{content: ""; clear: both; display: table;}[class*="col-"]{float: left; padding: 15px; border: 1px solid rgb(60, 182, 76);}.col-1{width: 50%;}</style> </head> <body> <div class="header">'
+    HeadTemplate = '<!DOCTYPE html><html> <head><script async src="https://www.googletagmanager.com/gtag/js?id=G-JM59LT7FPT"></script><script>window.dataLayer=window.dataLayer || []; function gtag(){dataLayer.push(arguments);}gtag(\'js\', new Date()); gtag(\'config\', \'G-JM59LT7FPT\');</script> <meta name="viewport" content="width=device-width, initial-scale=1.0"><meta property="og:image" content="https://kururugi.blob.core.windows.net/kururugi/screenshot.jpg"/><meta property="og:title" content="Vaccination Statistics Malaysia"/><meta property="og:description" content="Analysis and plotting of the official vaccination statistics data from JKJAV Malaysia"/> <style>*{box-sizing: border-box;}.header{padding: 15px;}.row::after{content: ""; clear: both; display: table;}[class*="col-"]{float: left; padding: 20px; border: 1px solid rgb(60, 182, 76);}.col-1{width: 50%;}</style><script type="text/javascript" src="plotly.js"></script> </head> <body style="background-color: #E1E8EB;"> <div class="header">'
     Close = '</div>'
     RowOpen = '<div class="row">'
     ColOpen = '<div class="col-1">'
@@ -125,7 +134,8 @@ def main(mytimer: func.TimerRequest) -> None:
         f.write(HeadTemplate)
         f.write("<h1>Vaccination Statistics Malaysia</h1>")
         f.write("<a href='https://kururugi.blob.core.windows.net/kururugi/about.html'>Technical details & about</a><br>Coded by: Amin Husni (aminhusni@gmail.com)<br><br>")
-        f.write("Data refreshed: " + current_time + " (MYT)<br>*For mobile, please use horizontal mode (rotate)")
+        f.write("Data refreshed: " + current_time + " (MYT)<br>")
+        f.write("Latest date in data: " + last_date_data + "<br>*For mobile, please use horizontal mode (rotate)<br>")
         f.write(Close)
 
         f.write(RowOpen)
@@ -162,8 +172,8 @@ def main(mytimer: func.TimerRequest) -> None:
         f.write("<br>Licenses: Official datapoint: <a href='https://www.data.gov.my/p/pekeliling-data-terbuka'>Pekeliling Pelaksanaan Data Terbuka Bil.1/2015 (Appendix B)</a>")
         f.write(Close)
 
-    connect_str = #REDACTED
-    container = #REDACTED
+    connect_str = # REDACTED
+    container = # REDACTED
 
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
     blob_client = blob_service_client.get_blob_client(container=container, blob="index.html")

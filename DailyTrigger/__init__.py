e7c4aa4c018c62d4ac31d60b4519ede60bef391a
Amin Husni <aminhusni@gmail.com>
2021-07-14 09:29:16 +0800

    Add Klang Valley
    Add Klang Valley calculations

--- DailyTrigger/__init__.py
+++ DailyTrigger/__init__.py
@@ -12,6 +12,8 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+
+
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
@@ -20,6 +22,10 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    # Static population total
+    population_total = 32657400
+
 
     # Get the current generation time in MYT timezone
     timeZ_My = pytz.timezone('Asia/Kuala_Lumpur')
@@ -81,7 +87,7 @@ def main(mytimer: func.TimerRequest) -> None:
     vaccination_rate.add_trace(go.Indicator(
                                             mode = "number+delta",
                                             value = df['dose2_daily'].iloc[-1:].item(),
-                                            number = {'prefix': "Today 2nd Dose "},
+                                            number = {'prefix': "Today 2nd Dose " },
                                             delta = {
                                                 'reference' : df['dose2_daily'].iloc[-2].item(),
                                                 "valueformat": ",.0f",
@@ -116,7 +122,7 @@ def main(mytimer: func.TimerRequest) -> None:
     vaccinated_total.add_trace(go.Indicator(
                                             mode = "number+delta",
                                             value = df['total_cumul'].iloc[-1:].item(),
-                                            number = {'prefix': "Total: " },
+                                            number = {'prefix': "Total " },
                                             delta = {
                                                 'reference' : df['total_cumul'].iloc[-2].item(),
                                                 "valueformat": ",0f"
@@ -124,13 +130,13 @@ def main(mytimer: func.TimerRequest) -> None:
                                             gauge = {
                                                 'axis': {'visible': False}
                                             },
-                                            domain = {'x': [0.05, 0.3], 'y': [0.60, 0.98]}
+                                            domain = {'x': [0.03, 0.35], 'y': [0.70, 0.95]}
     ))
 
     vaccinated_total.add_trace(go.Indicator(
                                             mode = "number+delta",
                                             value = df['dose1_cumul'].iloc[-1:].item(),
-                                            number = {'prefix': "1st Dose: " },
+                                            number = {'prefix': "1st Dose: "},
                                             delta = {
                                                 'reference' : df['dose1_cumul'].iloc[-2].item(),
                                                 "valueformat": ",.0f",
@@ -140,7 +146,7 @@ def main(mytimer: func.TimerRequest) -> None:
                                             gauge = {
                                                 'axis': {'visible': False}
                                             },
-                                            domain = {'x': [0.04, 0.3], 'y': [0.37, 0.60]}
+                                            domain = {'x': [0.03, 0.35], 'y': [0.40, 0.70]}
     ))
 
     vaccinated_total.add_trace(go.Indicator(
@@ -156,7 +162,7 @@ def main(mytimer: func.TimerRequest) -> None:
                                             gauge = {
                                                 'axis': {'visible': False}
                                             },
-                                            domain = {'x': [0.04, 0.3], 'y': [0.12, 0.5]}
+                                            domain = {'x': [0.03, 0.35], 'y': [0.20, 0.50]}
     ))
 
     series_names = ['Total Dose', '1st Dose', '2nd Dose']
@@ -168,7 +174,6 @@ def main(mytimer: func.TimerRequest) -> None:
     # Calculate the amount of people vaccinated vs unvaccinated
     # Get 1 dose total to date
     single_dose_total = df['dose1_cumul'].iloc[-1:].item()
-    population_total = 32764602
     # Get unvaccinated using maths lol #QuickMaths
     unvaccinated = population_total - single_dose_total
 
@@ -216,7 +221,7 @@ def main(mytimer: func.TimerRequest) -> None:
 
     title_graph = "Doses administered by day distribution from " + df_trim_week['date'].iloc[:1].item().strftime('%Y-%m-%d') + " to " + df_trim_week['date'].iloc[-1:].item().strftime('%Y-%m-%d')
 
-    # Plot the graph
+    # Plot the graph - Total doses administered to date
     day_trend = px.bar(df_trim_week, x='day_of_week', y='total_daily', 
                         category_orders={'day_of_week': ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]},
                         labels={
@@ -256,11 +261,31 @@ def main(mytimer: func.TimerRequest) -> None:
     df_trim_pop = df_trim.sort_values('state')
     df_trim_pop = df_trim_pop.reset_index()
 
+    # Calculate for Klang Valley (Selangor, KL, Putrajaya)
+    kv_pop = df_trim_pop[df_trim_pop['state'].isin(['Selangor', 'W.P. Kuala Lumpur',  'W.P. Putrajaya'])]
+    kv_pop = kv_pop.sum(axis=0)
+    kv_pop = kv_pop['pop'].item()
+
+    kv_dose = df_trim_dose[df_trim_dose['state'].isin(['Selangor', 'W.P. Kuala Lumpur',  'W.P. Putrajaya'])]
+    kv_dose = kv_dose.sum(axis=0)
+    kv_dose1 = kv_dose['dose1_cumul'].item()
+    kv_dose2 = kv_dose['dose2_cumul'].item()
+
+    # Insert into existing dataframe to draw the graph (df_trim_pop)
+    # Only values used for data representation are calculated & inserted for K.V 
+    # Insert Klang Valley into df_trim_pop
+    df_trim_pop.loc[-1] = ["17", "Klang Valley*¹", "17", kv_pop, "0", "0"]
+    # Insert Klang Valley into df_trim_dose
+    df_trim_dose.loc[-1] = ["-1", "today", "Klang Valley*¹", "0", "0", "0", kv_dose1, kv_dose2, kv_dose1+kv_dose2]
+
+    #df_trim_pop.loc[-1] = ["17", "Klang Valley", "17", kv_pop, "0", "0", "0", "0"]
+
     # Do some not very quick maths to get vaccinated percentage of each state
     df_trim_pop['vax1_pct'] = (df_trim_dose['dose1_cumul']/df_trim_pop['pop'])*100
     df_trim_pop['vax2_pct'] = (df_trim_dose['dose2_cumul']/df_trim_pop['pop'])*100
     df_trim_pop['unvax1'] = ((df_trim_pop['pop']-df_trim_dose['dose1_cumul'])/df_trim_pop['pop'])*100
     df_trim_pop['unvax2'] = ((df_trim_pop['pop']-df_trim_dose['dose2_cumul'])/df_trim_pop['pop'])*100
+
 
     state_dose1_pct = px.bar(df_trim_pop.sort_values('vax1_pct'), x=["vax1_pct", "unvax1"], y="state", 
                             labels={
@@ -363,12 +388,13 @@ def main(mytimer: func.TimerRequest) -> None:
         f.write(Close)
 
         f.write(RowOpen)
+        f.write("<br><i>*1 - Klang Valley in this calculation consists of WP Kuala Lumpur, Selangor and Putrajaya</i><br>")
         f.write("<br>Licenses: Official datapoint: <a href='https://www.data.gov.my/p/pekeliling-data-terbuka'>Pekeliling Pelaksanaan Data Terbuka Bil.1/2015 (Appendix B)</a> <br>")
         f.write("<a href='https://github.com/aminhusni/project_kururugi/blob/main/LICENSE'>Copyright (C) 2021 Amin Husni. MIT License</a><a href='https://kururugi.blob.core.windows.net/kururugi/about.html'>    More & Contact</a>")
         f.write(Close)
 
-    connect_str = # REDACTED
-    container = # REDACTED
+    connect_str = # Redacted
+    container = # Redacted
 
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
     blob_client = blob_service_client.get_blob_client(container=container, blob="index.html")

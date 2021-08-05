52b301421d3d0e774af16e5d3d30f53f86444465
Amin Husni <aminhusni@gmail.com>
2021-08-05 08:00:11 +0800

    Fix Overflow
    Fix percentage overflow representation on bar graph

--- DailyTrigger/__init__.py
+++ DailyTrigger/__init__.py
@@ -286,6 +286,8 @@ def main(mytimer: func.TimerRequest) -> None:
     df_trim_pop['unvax1'] = ((df_trim_pop['pop']-df_trim_dose['dose1_cumul'])/df_trim_pop['pop'])*100
     df_trim_pop['unvax2'] = ((df_trim_pop['pop']-df_trim_dose['dose2_cumul'])/df_trim_pop['pop'])*100
 
+    df_trim_pop['unvax1'] = (df_trim_pop['unvax1'] + abs(df_trim_pop['unvax1'])) / 2
+    df_trim_pop['unvax2'] = (df_trim_pop['unvax2'] + abs(df_trim_pop['unvax2'])) / 2
 
     state_dose1_pct = px.bar(df_trim_pop.sort_values('vax1_pct'), x=["vax1_pct", "unvax1"], y="state", 
                             labels={
